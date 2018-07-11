319134ec5e1bcf049f36fe70982a2dc3fcacdd6d
Gary Calle <gary.sk8@icloud.com>
2018-07-11 15:51:54 -0500

    first

---
+++ dircetur/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Direccion Regional de Comercio Exterior y Turismo</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<link rel="stylesheet" type="text/css" href="./fonts.css">
+	<link rel="icon" type="image/png" href="./images/escudo.png">
+</head>
+<body>
+	<header>
+		<img src="./images/LOGODIRCETURCOLOR.png" width="1154" height="285">
+		<nav id="navegador">
+			<ul>
+				<li><a href="#"><span class="icon-home"></span>Inicio</a></li>
+				<li><a href="#"><span class="icon-library"></span>Institucional</a></li>
+				<li><a href="#"><span class="icon-folder-open"></span>Gestion</a></li>
+				<li><a href="#"><span class="icon-earth"></span>Comercio Exterior</a></li>
+				<li><a href="#"><span class="icon-office"></span>Turismo</a></li>
+				<li><a href="#"><span class="icon-basecamp"></span>Artesania</a></li>
+				<li><a href="#"><span class="icon-profile"></span>Datos Generales</a></li>
+				<li><a href="#"><span class="icon-user-tie"></span>Contactenos</a></li>
+				
+			</ul>
+		</nav>
+	</header>
+	
+
+</body>
+</html>
