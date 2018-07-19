d9c84ffaff451a73976fb5ee78ba302a2b18eae3
Gary Calle <joevermelho@protonmail.com>
2018-07-18 22:36:23 -0500

    segunda parte
    social bar
    nav bar

--- dircetur/index.php
+++ dircetur/index.php
@@ -2,27 +2,47 @@
 <html>
 <head>
 	<title>Direccion Regional de Comercio Exterior y Turismo</title>
-	<link rel="stylesheet" type="text/css" href="./style.css">
-	<link rel="stylesheet" type="text/css" href="./fonts.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/navbar.css">
+	<link rel="stylesheet" href="js/navbar_toggle.js">
+	<link rel="stylesheet" href="css/socialbar.css">	
+	<meta name="viewport" content="wight=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="./images/escudo.png">
 </head>
 <body>
-	<header>
-		<img src="./images/LOGODIRCETURCOLOR.png" width="1154" height="285">
-		<nav id="navegador">
-			<ul>
-				<li><a href="#"><span class="icon-home"></span>Inicio</a></li>
-				<li><a href="#"><span class="icon-library"></span>Institucional</a></li>
-				<li><a href="#"><span class="icon-folder-open"></span>Gestion</a></li>
-				<li><a href="#"><span class="icon-earth"></span>Comercio Exterior</a></li>
-				<li><a href="#"><span class="icon-office"></span>Turismo</a></li>
-				<li><a href="#"><span class="icon-basecamp"></span>Artesania</a></li>
-				<li><a href="#"><span class="icon-profile"></span>Datos Generales</a></li>
-				<li><a href="#"><span class="icon-user-tie"></span>Contactenos</a></li>
-				
-			</ul>
-		</nav>
-	</header>
+	<div class="slider">
+		
+	</div>
+	<div class="navbar" id="navbarId">
+		<a href="#inicio" class="fa fa-facebook">Inicio</a>
+		<a href="#inicio">Institucional</a>
+		<a href="#inicio">Gestion</a>
+		<a href="#inicio">Comercion</a>
+		<a href="#inicio">Turismo</a>
+		<a href="#inicio">Artesania</a>
+		<a href="#inicio">Datos Generales</a>
+		<a href="#inicio">Contactenos</a>
+		<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    		<i class="fa fa-bars"></i>
+    	</a>
+
+    	<script>
+			function myFunction() {
+			    var x = document.getElementById("navbarId");
+			    if (x.className === "navbar") {
+			        x.className += " responsive";
+			    } else {
+			        x.className = "navbar";
+			    }
+			}
+		</script>
+	</div>
+
+	<div class="socialbar">
+		<a href="https://www.facebook.com/dircetur.regiontacna/" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
+		<a href="https://www.youtube.com/channel/UC9vpMPOsoDL8m0YIwdD_MTA" target="_blank" class="youtube"><i class="fa fa-youtube"></i></a>				
+	</div>
+	
 	
 
 </body>
